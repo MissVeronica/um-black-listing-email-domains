115e4723019b7d8aa424cc843a4c29faea6a812e
MissVeronica <veronica@tranza.se>
2024-11-07 05:10:03 +0100

    Update um_blocked_email_domains.php

--- um_blocked_email_domains.php
+++ um_blocked_email_domains.php
@@ -2,15 +2,17 @@
 /**
  * Plugin Name:     Ultimate Member - Blocked Emails and Domains
  * Description:     Extension to Ultimate Member for additional blocking possibilities like subdomains and top level domains and disposable email domains.
- * Version:         2.0.0
+ * Version:         3.0.0
  * Requires PHP:    7.4
  * Author:          Miss Veronica
  * License:         GPL v2 or later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
- * Author URI:      https://github.com/MissVeronica
+ * Author URI:      https://github.com/MissVeronica?tab=repositories
+ * Plugin URI:      https://github.com/MissVeronica/um-black-listing-email-domains
+ * Update URI:      https://github.com/MissVeronica/um-black-listing-email-domains
  * Text Domain:     ultimate-member
  * Domain Path:     /languages
- * UM version:      2.5.0
+ * UM version:      2.8.9
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; 
@@ -19,14 +21,13 @@ if ( ! class_exists( 'UM' ) ) return;
 class UM_Blocked_Emails_Domains {
 
     public $user_blocked_emails = array();
-    public $disposable_domains  = array();
 
     function __construct() {
 
-        add_action( 'um_submit_form_errors_hook__blockedemails', array( $this, 'um_submit_form_customized_blockedemails' ), 10, 1 );
-        remove_action( 'um_submit_form_errors_hook__blockedemails', 'um_submit_form_errors_hook__blockedemails', 10 );
+        add_action(    'um_submit_form_errors_hook__blockedemails', array( $this, 'um_submit_form_customized_blockedemails' ), 10, 1 );
+        remove_action( 'um_submit_form_errors_hook__blockedemails',               'um_submit_form_errors_hook__blockedemails' );
 
-        add_filter( 'um_custom_error_message_handler', array( $this, 'um_custom_error_message_handler_blockedemails' ), 10, 2 );
+        add_filter( 'um_custom_error_message_handler',              array( $this, 'um_custom_error_message_handler_blockedemails' ), 10, 2 );
     }
 
     public function um_submit_form_customized_blockedemails( $args ) {
@@ -34,31 +35,15 @@ class UM_Blocked_Emails_Domains {
         $blocked_emails = UM()->options()->get( 'blocked_emails' );
 
         if ( ! empty( $blocked_emails )) {
-            $this->user_blocked_emails = array_map( 'rtrim', explode( "\n", sanitize_text_field( strtolower( $blocked_emails )) ) );
-        } 
-
-        $disposable_email_file = UM()->uploader()->get_upload_base_dir() . 'disposable_emails' . DIRECTORY_SEPARATOR . 'disposable_emails_list.txt';
-        
-        if ( file_exists(  $disposable_email_file )) {
-
-            $disposable_text = file_get_contents( $disposable_email_file );
-            if ( ! empty( $disposable_text )) {
-
-                $this->disposable_domains = array_map( 'sanitize_text_field', preg_split( '/[\r\n]+/', $disposable_text, -1, PREG_SPLIT_NO_EMPTY ));
-                $this->disposable_domains = array_map( 'strtolower', array_map( 'trim', $this->disposable_domains ));
-            }
-        } 
-
-        if ( empty( $this->disposable_domains ) && empty( $this->emails )) {
-            return;
+            $this->user_blocked_emails = array_map( 'rtrim', explode( "\n", sanitize_text_field( strtolower( $blocked_emails ))));
         }
 
-        if ( isset( $args['user_email'] ) ) {
+        if ( isset( $args['user_email'] ) && is_email( $args['user_email'] )) {
 
             $this->validate_email( strtolower( $args['user_email'] ));
         }
 
-        if ( isset( $args['username'] ) ) {
+        if ( isset( $args['username'] ) && is_email( $args['username'] )) {
 
             $this->validate_email( strtolower( $args['username'] ));
         }
@@ -66,38 +51,50 @@ class UM_Blocked_Emails_Domains {
 
     public function validate_email( $args ) {
 
-        if ( is_email( $args )) {
+        $domain = explode( '@', $args );
+        $emailDomain = $domain[1];
 
-            if ( in_array( $args, $this->user_blocked_emails ) ) {
-                exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_email' ) ) ) );
+        if ( ! empty( $this->user_blocked_emails )) {
+
+            if ( in_array( $args, $this->user_blocked_emails )) {
+                exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_email' ))));
             }
 
-            $domain = explode( '@', $args );
             $check_domain = str_replace( $domain[0], '*', $args );
 
-            if ( in_array( $check_domain, $this->user_blocked_emails ) ) {
-                exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_domain' ) ) ) );
+            if ( in_array( $check_domain, $this->user_blocked_emails )) {
+                exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_domain' ))));
             }
 
-            if ( in_array( $domain[1], $this->disposable_domains )) {
-                exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_disposable_domain' ) ) ) );
-            }
-
-            $tld = explode( '.', $domain[1] );
+            $tld = explode( '.', $emailDomain );
             $domain_index = count( $tld ) - 1;
             $check_tld = '*.' . $tld[$domain_index]; 
 
-            if ( in_array( $check_tld, $this->user_blocked_emails ) ) {
+            if ( in_array( $check_tld, $this->user_blocked_emails )) {
                 exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_top_level_domain' ) ) ) );
             }
 
             while( count( $tld ) > 2 ) {
+
                 array_shift( $tld );
                 $subdomain = '*.' . implode( '.', $tld );
 
-                if ( in_array( $subdomain, $this->user_blocked_emails ) ) {
+                if ( in_array( $subdomain, $this->user_blocked_emails )) {
                     exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_subdomain' ) ) ) );
                 }
+            }
+        }
+
+        $allowDomains = file_get_contents( 'https://raw.githubusercontent.com/amieiro/disposable-email-domains/master/allowDomains.json' );
+        $allowDomains = json_decode( $allowDomains, true );
+
+        if ( ! in_array( $emailDomain, $allowDomains )) {
+
+            $denyDomains = file_get_contents( 'https://raw.githubusercontent.com/amieiro/disposable-email-domains/master/denyDomains.json' );
+            $denyDomains = json_decode( $denyDomains, true );
+
+            if ( in_array( $emailDomain, $denyDomains )) {
+                exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_disposable_domain' ))));
             }
         }
     }
@@ -106,9 +103,9 @@ class UM_Blocked_Emails_Domains {
 
         switch ( $request_err ) {
 
-            case 'blocked_top_level_domain':    $err =  __( 'We do not accept registrations from this top level email domain.', 'ultimate-member' ); break;
-            case 'blocked_subdomain':           $err =  __( 'We do not accept registrations from this email subdomain.', 'ultimate-member' ); break;
-            case 'blocked_disposable_domain':   $err =  __( 'We do not accept registrations from this temporary email domain.', 'ultimate-member' ); break;
+            case 'blocked_top_level_domain':    $err = esc_html__( 'We do not accept registrations from this top level email domain.', 'ultimate-member' ); break;
+            case 'blocked_subdomain':           $err = esc_html__( 'We do not accept registrations from this email subdomain.',        'ultimate-member' ); break;
+            case 'blocked_disposable_domain':   $err = esc_html__( 'We do not accept registrations from this temporary email domain.', 'ultimate-member' ); break;
             default: break;
         }
 
